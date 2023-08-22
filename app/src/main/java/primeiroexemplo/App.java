37f477bfbed1492735a79a95bff9b3ef2d3418df
danis borges <borgesdanielli81@gmail.com>
2023-08-22 18:38:41 +0000

    consoleapp

---
+++ app/src/main/java/primeiroexemplo/App.java
@@ -0,0 +1,7 @@
+package primeiroexemplo;
+
+public class App {
+    public static void main (String[] args) { //método main indica que é uma classe principal
+        System.out.println("Olá Gradle!!!");
+    }
+}
